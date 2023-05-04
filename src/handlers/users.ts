dd9dbcff747155fa141586cd1f07448fef124c9e
mikias.wk <mikias.wk@2f-capital.com>
2023-05-04 15:33:17 +0300

    allow user to signin

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,16 +1,36 @@
 import { createHash } from "crypto";
 import { Request, Response } from "express";
 import prisma from "../db";
-import { createJWT } from "../modules/auth";
+import { comparePasswords, createJWT } from "../modules/auth";
 
 export const createUser = async (req: Request, res: Response) => {
     const hassPassword = createHash(req.body.password)
 
-    const user =  await prisma.user.create({
+    const user = await prisma.user.create({
         data: { ...req.body, password: hassPassword }
     })
 
     const token = createJWT(user)
 
-    res.json({token})
+    res.json({ token })
+}
+
+export const signin = async (req: Request, res: Response) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            email: req.body.email
+        }
+    })
+
+    if (user) {
+        const isValid = await comparePasswords(req.body.password as string, user?.password as string)
+        if (!isValid) {
+            res.status(401).json({ message: 'invalid credentials' })
+        }
+
+        const token = createJWT(user)
+        res.json({ token })
+    }
+
+    res.status(404).json({ message: 'User not found' })
 }
