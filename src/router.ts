842a0cef88bc77f79de03de09beb5c785265bea7
mikias.wk <mikias.wk@2f-capital.com>
2023-05-05 14:51:30 +0300

    add input validation to routes

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,8 @@ import { Router, Response, Request } from "express";
 import bcrpt from 'bcrypt'
 import { createUser, signin } from "./handlers/users";
 import { protect } from "./modules/auth";
+import { body, oneOf, validationResult } from "express-validator"
+import { handleInputErrors } from "./modules/middleware";
 const router = Router()
 
 /**
@@ -14,57 +16,80 @@ router.post('/signin', signin)
  * Product
  */
 router.use(protect)
-router.get('/products', (req:Request, res: Response)=>{
-    res.json({message: 'hey'}).status(200)
+router.get('/products', (req: Request, res: Response) => {
+    res.json({ message: 'hey' }).status(200)
 })
-router.get('/products/:id', (req: any, res: Response)=>{
-    res.json({id: req.params.id, testt: req?.test}).status(200)
+router.get('/products/:id', (req: any, res: Response) => {
+    res.json({ id: req.params.id, testt: req?.test }).status(200)
 })
-router.put('/products/:id', (req: Request, res: Response)=>{
+router.put('/products/:id', body('name').exists().optional(), handleInputErrors, (req: Request, res: Response) => {
 
 })
-router.post('/products', (req: Request, res: Response)=>{
+router.post('/products', (req: Request, res: Response) => {
 
 })
-router.delete('/products/:id', (req: Request, res: Response)=>{
+router.delete('/products/:id', (req: Request, res: Response) => {
 
 })
 
 /**
  * Update
  */
- router.get('/updates', (req:Request, res: Response)=>{
+router.get('/updates', (req: Request, res: Response) => {
 
 })
-router.get('/updates/:id', (req: Request, res: Response)=>{
+router.get('/updates/:id', (req: Request, res: Response) => {
 
 })
-router.put('/updates/:id', (req: Request, res: Response)=>{
+router.put(
+    '/updates/:id',
+    body('name').optional(),
+    body('body').optional(),
+    body('status').isIn([body('IN_PROGRESS'), body('SHIPPED'), body('DEPRECATED')]),
+    handleInputErrors,
+    (req: Request, res: Response) => {
 
-})
-router.post('/updates', (req: Request, res: Response)=>{
+    })
+router.post(
+    '/updates',
+    body('name').exists().isString(),
+    body('body').exists().isString(),
+    body('productId').exists().isString(),
+    handleInputErrors,
+    (req: Request, res: Response) => {
 
-})
-router.delete('/updates/:id', (req: Request, res: Response)=>{
+    })
+router.delete('/updates/:id', (req: Request, res: Response) => {
 
 })
 
 /**
  * Update Point
  */
- router.get('/updatepoints', (req:Request, res: Response)=>{
+router.get('/updatepoints', (req: Request, res: Response) => {
 
 })
-router.get('/updatepoints/:id', (req: Request, res: Response)=>{
+router.get('/updatepoints/:id', (req: Request, res: Response) => {
 
 })
-router.put('/updatepoints/:id', (req: Request, res: Response)=>{
+router.put(
+    '/updatepoints/:id',
+    body('name').isString().optional(),
+    body('description').isString().optional(),
+    handleInputErrors,
+    (req: Request, res: Response) => {
 
-})
-router.post('/updatepoints', (req: Request, res: Response)=>{
+    })
+router.post(
+    '/updatepoints',
+    body('name').exists().isString(),
+    body('descriptioin').exists().isString(),
+    body('updateId').exists().isString(),
+    handleInputErrors,
+    (req: Request, res: Response) => {
 
-})
-router.delete('/updatepoints/:id', (req: Request, res: Response)=>{
+    })
+router.delete('/updatepoints/:id', (req: Request, res: Response) => {
 
 })
 
