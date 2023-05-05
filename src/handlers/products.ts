540f2faf2265340eab92ba8a6a2b1104067e878b
mikias.wk <mikias.wk@2f-capital.com>
2023-05-05 16:26:48 +0300

    add product services - create, update, delete product

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -30,3 +30,50 @@ export const getProduct = async (req: Request & { user: Partial<User> }, res: Re
 
     res.json({ data: product })
 }
+
+// Create a product
+export const createProduct = async (req: Request & { user: Partial<User> }, res: Response) => {
+    const product = await prisma.product.create({
+        data: {
+            name: req.body.name,
+            user: {
+                connect: {
+                    id: req.user.id
+                }
+            }
+        }
+    })
+
+    res.status(201).json({ data: product })
+}
+
+// Update a product
+export const updateProduct = async (req: Request & { user: User }, res: Response) => {
+    const product = await prisma.product.update({
+        where: {
+            id_userId: {
+                id: req.params.id,
+                userId: req?.user?.id
+            }
+        },
+        data: {
+            name: req.body.name,
+        }
+    })
+
+    res.json({ data: product })
+}
+
+// Delete a product
+export const deleteProduct = async (req: Request & { user: User }, res: Response) => {
+    const product = await prisma.product.delete({
+        where: {
+            id_userId: {
+                id: req.params.id,
+                userId: req.user.id
+            }
+        }
+    })
+
+    res.json({ message: 'product deleted successfully', data: product })
+}
