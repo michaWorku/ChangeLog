f1da0fe53628f9519c4c65765a465b180d36c645
mikias.wk <mikias.wk@2f-capital.com>
2023-05-08 17:12:17 +0300

    catch error for all handlers

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -1,79 +1,99 @@
-import { Request, Response } from "express"
+import { NextFunction, Request, Response } from "express"
 import prisma from "../db"
 import { User } from "@prisma/client"
 
 // Get all user products
-export const getProducts = async (req: Request & { user: Partial<User> }, res: Response) => {
-    const user = await prisma.user.findUnique({
-        where: {
-            id: req?.user?.id
-        },
-        include: {
-            products: true
-        }
-    })
-
-    res.json({ data: user?.products })
+export const getProducts = async (req: Request & { user: Partial<User> }, res: Response, next: NextFunction) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: req?.user?.id
+            },
+            include: {
+                products: true
+            }
+        })
+    
+        res.json({ data: user?.products })
+    } catch (error) {
+        next(error)
+    }
 }
 
 // Get a product by id
-export const getProduct = async (req: Request & { user: Partial<User> }, res: Response) => {
-    const product = await prisma.product.findFirst({
-        where: {
-            id: req?.params?.id,
-            userId: req.user.id
-        },
-        include: {
-            updates: true
-        }
-    })
-
-    res.json({ data: product })
+export const getProduct = async (req: Request & { user: Partial<User> }, res: Response, next: NextFunction) => {
+    try {
+        const product = await prisma.product.findFirst({
+            where: {
+                id: req?.params?.id,
+                userId: req.user.id
+            },
+            include: {
+                updates: true
+            }
+        })
+    
+        res.json({ data: product })
+    } catch (error) {
+        next(error)
+    }
 }
 
 // Create a product
-export const createProduct = async (req: Request & { user: Partial<User> }, res: Response) => {
-    const product = await prisma.product.create({
-        data: {
-            name: req.body.name,
-            user: {
-                connect: {
-                    id: req.user.id
+export const createProduct = async (req: Request & { user: Partial<User> }, res: Response, next: NextFunction) => {
+    try {
+        const product = await prisma.product.create({
+            data: {
+                name: req.body.name,
+                user: {
+                    connect: {
+                        id: req.user.id
+                    }
                 }
             }
-        }
-    })
-
-    res.status(201).json({ data: product })
+        })
+    
+        res.status(201).json({ data: product })
+    } catch (err: any) {
+        next(err)
+    }
 }
 
 // Update a product
-export const updateProduct = async (req: Request & { user: User }, res: Response) => {
-    const product = await prisma.product.update({
-        where: {
-            id_userId: {
-                id: req.params.id,
-                userId: req?.user?.id
+export const updateProduct = async (req: Request & { user: User }, res: Response, next: NextFunction) => {
+    try {
+        const product = await prisma.product.update({
+            where: {
+                id_userId: {
+                    id: req.params.id,
+                    userId: req?.user?.id
+                }
+            },
+            data: {
+                name: req.body.name,
             }
-        },
-        data: {
-            name: req.body.name,
-        }
-    })
-
-    res.json({ data: product, message: 'product updated successfully' })
+        })
+    
+        res.json({ data: product, message: 'product updated successfully' })
+    } catch (error) {
+       next(error) 
+    }
 }
 
 // Delete a product
-export const deleteProduct = async (req: Request & { user: User }, res: Response) => {
-    const product = await prisma.product.delete({
-        where: {
-            id_userId: {
-                id: req.params.id,
-                userId: req.user.id
+export const deleteProduct = async (req: Request & { user: User }, res: Response, next: NextFunction) => {
+    try {
+        const product = await prisma.product.delete({
+            where: {
+                id_userId: {
+                    id: req.params.id,
+                    userId: req.user.id
+                }
             }
-        }
-    })
-
-    res.json({ message: 'product deleted successfully', data: product })
+        })
+    
+        res.json({ message: 'product deleted successfully', data: product })
+    } catch (error) {
+       next(error) 
+    }
 }
--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,36 +1,45 @@
 import { createHash } from "crypto";
-import { Request, Response } from "express";
+import e, { NextFunction, Request, Response } from "express";
 import prisma from "../db";
 import { comparePasswords, createJWT, hashPassword } from "../modules/auth";
 
-export const createUser = async (req: Request, res: Response) => {
-    const hashedPassword = await hashPassword(req.body.password)
+export const createUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const hashedPassword = await hashPassword(req.body.password)
 
-    const user = await prisma.user.create({
-        data: { ...req.body, password: hashedPassword }
-    })
-
-    const token = createJWT(user)
-
-    res.status(201).json({ token })
-}
-
-export const signin = async (req: Request, res: Response) => {
-    const user = await prisma.user.findUnique({
-        where: {
-            email: req.body.email
-        }
-    })
-
-    if (user) {
-        const isValid = await comparePasswords(req.body.password, user?.password)
-        if (!isValid) {
-            res.status(401).json({ message: 'invalid credentials' })
-        }
+        const user = await prisma.user.create({
+            data: { ...req.body, password: hashedPassword }
+        })
 
         const token = createJWT(user)
-        res.status(200).json({ token })
-    } else{
-        res.status(404).json({ message: 'User not found' })
+
+        res.status(201).json({ token })
+    } catch (error: any) {
+        error.type = 'input'
+        next(error)
+    }
+}
+
+export const signin = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email: req.body.email
+            }
+        })
+    
+        if (user) {
+            const isValid = await comparePasswords(req.body.password, user?.password)
+            if (!isValid) {
+                res.status(401).json({ message: 'invalid credentials' })
+            }
+    
+            const token = createJWT(user)
+            res.status(200).json({ token })
+        } else {
+            res.status(404).json({ message: 'User not found' })
+        }
+    } catch (error) {
+        next(error)
     }
 }
