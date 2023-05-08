16a3053e840f12518cbb3857f5d74569f7b33068
mikias.wk <mikias.wk@2f-capital.com>
2023-05-08 11:06:09 +0300

    bug-fix  and next to handle input errors

--- src/modules/middleware.ts
+++ src/modules/middleware.ts
@@ -5,5 +5,7 @@ export const handleInputErrors = (req: Request, res: Response, next: NextFunctio
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         res.status(400).json({ errors: errors.array() })
+    }{
+        next()
     }
 }
